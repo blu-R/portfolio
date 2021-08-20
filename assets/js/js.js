27c8cc5fb4b96d5de3dd5aea7b4f986e9a64c3cc
bLu.R <blu.r@outlook.com>
2021-08-20 17:18:05 -0500

    Hide/Unhide elementos v1

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,32 @@
-const btnCheckWork = document.querySelector(".button .check-work");
+const homeTitle = document.querySelector("#home h1");
+const homeSubtitle = document.querySelector("#home h2");
+const btnCheckWork = document.querySelector(".check-work");
 
 function toggleNav() {
   const nav = document.getElementById("main-nav");
   nav.classList.toggle("visible");
 }
 
-function name(params) {}
+function scrollHideElements() {
+  let offsetHomeTitle = homeTitle.offsetTop;
+  let offsetHomeSubtitle = homeSubtitle.offsetTop;
+  let offsetBtnCheckWork = btnCheckWork.offsetTop;
+
+  if (window.scrollY > offsetHomeTitle - 30) {
+    homeTitle.classList.add("hidden");
+  } else {
+    homeTitle.classList.remove("hidden");
+  }
+  if (window.scrollY > offsetHomeSubtitle - 80) {
+    homeSubtitle.classList.add("hidden");
+  } else {
+    homeSubtitle.classList.remove("hidden");
+  }
+  if (window.scrollY > offsetBtnCheckWork - 60) {
+    btnCheckWork.classList.add("hidden");
+  } else {
+    btnCheckWork.classList.remove("hidden");
+  }
+}
+
+document.addEventListener("scroll", scrollHideElements);
