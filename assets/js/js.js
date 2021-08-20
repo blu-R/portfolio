dec548bd0e456e09d9d77af824370236f9560a2c
bLu.R <blu.r@outlook.com>
2021-08-20 14:58:54 -0500

    99% listo

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,8 @@
+const btnCheckWork = document.querySelector(".button .check-work");
+
 function toggleNav() {
   const nav = document.getElementById("main-nav");
   nav.classList.toggle("visible");
 }
+
+function name(params) {}
